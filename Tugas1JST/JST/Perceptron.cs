3d1accb72eea802799147e10686cc81aa2c485a2
karmidin <if09071@gmail.com>
2017-07-27 16:19:35 +0700

    Yoos

--- Tugas1JST/JST/Perceptron.cs
+++ Tugas1JST/JST/Perceptron.cs
@@ -31,7 +31,7 @@ namespace JST
         {
             int countEpoch = 1;
             bool belajar = true;
-            hasilTraining.Append("Hasil Training Yoo\n");
+            hasilTraining.Append("Hasil Training Yoos\n");
             printExcel+="Hasil Training\n";
             while (isLearn())
             {
